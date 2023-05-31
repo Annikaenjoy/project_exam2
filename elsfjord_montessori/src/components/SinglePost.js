ebf08500930b41fedf7a89a3f5e460b8d6eb58e7
Annika Engøy <annikaa@live.no>
2023-06-01 00:38:04 +0200

    Single post with styling

--- elsfjord_montessori/src/components/SinglePost.js
+++ elsfjord_montessori/src/components/SinglePost.js
@@ -8,41 +8,61 @@ import { apiUrl } from "../constants/Api";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 const SinglePost = (props) => {
-  const [post, setPost] = useState([]);
+  const [post, setPost] = useState(null);
+  const [featuredMedia, setFeaturedMedia] = useState(null);
 
   const http = useAxios();
-
   let { id } = useParams();
-
-  const getPostUrl = apiUrl + `wp/v2/posts/${id}`;
+  const getPostUrl = `${apiUrl}wp/v2/posts/${id}?_embed`;
 
   useEffect(() => {
     async function getPost() {
       try {
         const result = await http.get(getPostUrl);
-
         console.log(result.data);
         setPost(result.data);
+
+        if (
+          result.data._embedded &&
+          result.data._embedded["wp:featuredmedia"]
+        ) {
+          const featuredMediaUrl =
+            result.data._embedded["wp:featuredmedia"][0].source_url;
+          setFeaturedMedia(featuredMediaUrl);
+        }
       } catch (error) {
         console.log(error);
       }
     }
     getPost();
   }, []);
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <Container fluid>
-        <Row className="justify-content-center align-items-center">
-          <Col md={10}>
-            {/* <h2>{post.title.rendered}</h2>
-            <p>{post.date}</p>
-            <p>{post.content.rendered}</p> */}
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container fluid>
+      <Row className="justify-content-center align-items-center">
+        <Col className="post_content" md={10}>
+          {" "}
+          {featuredMedia && (
+            <Image
+              className="single_post_img"
+              src={featuredMedia}
+              alt={post.title.rendered}
+              fluid
+            />
+          )}
+          <h2 className="single_post_title">{post.title.rendered}</h2>
+          <p className="post_date">{post.date}</p>
+          <p>{post.content.rendered}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
