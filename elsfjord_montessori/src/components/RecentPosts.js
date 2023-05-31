305b53b7cf5b54aacea1874d2639565c9fab8e5b
Annika Engøy <annikaa@live.no>
2023-05-31 02:15:18 +0200

    Recent posts

--- elsfjord_montessori/src/components/RecentPosts.js
+++ elsfjord_montessori/src/components/RecentPosts.js
@@ -1,23 +1,75 @@
 import React from "react";
+import { apiUrl, postUrl } from "../constants/Api";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
+// React Bootstrap
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
+const newUrl = `${apiUrl}${postUrl}?_embed&per_page=3`;
+
 const RecentPosts = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getPosts() {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const response = await fetch(newUrl);
+        const result = await response.json();
+
+        setPosts(result);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+      }
+    }
+
+    getPosts();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>An error occurred when calling the API</div>;
+  }
+
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Title</Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
+      <Container fluid>
+        <Row className="justify-content-center">
+          {posts.map((post) => (
+            <Col className="blogposts" xs={10} md={3} key={post.id}>
+              <Link to={`/post/${post.id}`}>
+                <Card className="post_card_frontpage">
+                  <Card.Body>
+                    {post._embedded &&
+                      post._embedded["wp:featuredmedia"] &&
+                      post._embedded["wp:featuredmedia"][0].source_url && (
+                        <Card.Img
+                          className="post_img"
+                          variant="top"
+                          src={post._embedded["wp:featuredmedia"][0].source_url}
+                          alt={post.title.rendered}
+                        />
+                      )}
+                    <Card.Title className="post_title_frontpage">
+                      {post.title.rendered}
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          ))}
         </Row>
       </Container>
     </>
