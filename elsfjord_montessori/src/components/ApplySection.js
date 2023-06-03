58a32ece635d391952291a038e7b694e141b29d5
Annika Engøy <annikaa@live.no>
2023-06-03 22:33:23 +0200

    Updated gallery images

--- elsfjord_montessori/src/components/ApplySection.js
+++ elsfjord_montessori/src/components/ApplySection.js
@@ -7,7 +7,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 // Images
-import Klippoglim from "../assets/img/Klippoglim_web.jpg";
+import Perler from "../assets/img/perlematerial_web.jpg";
 
 const ApplySection = () => {
   let navigate = useNavigate();
@@ -19,7 +19,7 @@ const ApplySection = () => {
       <Container fluid className="section_container">
         <Row className="justify-content-center align-items-center">
           <Col xs={10} md={12} lg={6}>
-            <img className="section_img" src={Klippoglim} />
+            <img className="section_img" src={Perler} />
           </Col>{" "}
           <Col xs={10} md={12} lg={6}>
             <h2 className="h2_margin">Søknad om skoleplass</h2>
--- elsfjord_montessori/src/components/Gallery.js
+++ elsfjord_montessori/src/components/Gallery.js
@@ -6,9 +6,19 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
-// Images
-import Perler from "../assets/img/perlematerial_web.jpg";
+// Components
 import Heading from "./Heading";
+
+// Images
+import Leafs from "../assets/img/Leafs_web.jpg";
+import Learning from "../assets/img/Learning_web.jpg";
+import LeafPainting from "../assets/img/Leaf_painting_web.jpg";
+import Math from "../assets/img/Math_numbers_web.jpg";
+import Insects from "../assets/img/Playing_insects_web.jpg";
+import NaturalMaterials from "../assets/img/Natural_materials_web.jpg";
+import Magnifying from "../assets/img/Magnifying_nature_web.jpg";
+import Drawing from "../assets/img/Drawing_plant_web.jpg";
+
 const Gallery = (props) => {
   return (
     <>
@@ -16,30 +26,30 @@ const Gallery = (props) => {
         <Heading>Galleri</Heading>
         <Row>
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={Leafs} fluid />
           </Col>
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={Learning} fluid />
           </Col>
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={LeafPainting} fluid />
           </Col>{" "}
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={Math} fluid />
           </Col>
         </Row>
         <Row>
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={Insects} fluid />
           </Col>{" "}
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={NaturalMaterials} fluid />
           </Col>{" "}
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={Drawing} fluid />
           </Col>{" "}
           <Col className="gallery_col" md={3}>
-            <Image src={Perler} fluid />
+            <Image src={Magnifying} fluid />
           </Col>
         </Row>
       </Container>
